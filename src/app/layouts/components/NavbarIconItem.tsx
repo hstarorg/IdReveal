06420af6e56c5bec3685d5a286dc4392d7b151f0
hstarorg <hm910705@163.com>
2025-07-13 17:05:59 +0800

    feat: optimize tooltip effect

--- src/app/layouts/components/NavbarIconItem.tsx
+++ src/app/layouts/components/NavbarIconItem.tsx
@@ -7,11 +7,13 @@ export type NavbarIconItemProps = {
 
 export function NavbarIconItem(props: PropsWithChildren<NavbarIconItemProps>) {
   return (
-    <div className="h-11 w-11 flex items-center justify-center cursor-pointer rounded-full hover:bg-gray-200 transition-colors">
-      <Tooltip>
-        <TooltipTrigger>{props.children}</TooltipTrigger>
-        <TooltipContent>{props.tooltip}</TooltipContent>
-      </Tooltip>
-    </div>
+    <Tooltip>
+      <TooltipTrigger>
+        <div className="h-11 w-11 flex items-center justify-center cursor-pointer rounded-full hover:bg-gray-100 transition-colors">
+          {props.children}
+        </div>
+      </TooltipTrigger>
+      <TooltipContent>{props.tooltip}</TooltipContent>
+    </Tooltip>
   );
 }
