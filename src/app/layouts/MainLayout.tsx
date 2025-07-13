7bc16dbed61aceaec660962627c2a954fa230cd4
hstarorg <hm910705@163.com>
2025-07-13 16:49:21 +0800

    feat: add source and submit bug link

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBug } from '@fortawesome/free-solid-svg-icons';
 
 export function MainLayout() {
   return (
@@ -10,11 +11,28 @@ export function MainLayout() {
           <img src="/logo.svg" alt="Logo" className="h-11 w-11 mr-2" />
           <h1 className="text-xl font-bold text-gray-900">ID Reveal</h1>
         </div>
-        {/* <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center">
           <div className="h-11 w-11 flex items-center justify-center cursor-pointer rounded-full hover:bg-gray-200 transition-colors">
-            <FontAwesomeIcon icon={faLanguage} />
+            <a
+              href="https://github.com/hstarorg/IdReveal/issues"
+              title="Submit a bug"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faBug} />
+            </a>
           </div>
-        </div> */}
+          <div className="h-11 w-11 flex items-center justify-center cursor-pointer rounded-full hover:bg-gray-200 transition-colors">
+            <a
+              href="https://github.com/hstarorg/IdReveal"
+              title="Source on Github"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </div>
+        </div>
       </header>
       <main>
         <Outlet />
